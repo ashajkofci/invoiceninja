53a3dc696f9fbb75750a5666ba9e4feb18f7b345
Adrian Shajkofci <adrianshajkofci@MacBook-Air-de-Adrian.local>
2026-09-13 23:05:11 +0200

    Update react

--- resources/views/react/index.blade.php
+++ resources/views/react/index.blade.php
@@ -25,7 +25,7 @@
     <link rel="icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" href="/logo180.png" />
     <link rel="manifest" href="/manifest.json" />
-    <script type="module" crossorigin src="/react/index-BBqD5yiH.js"></script>
+    <script type="module" crossorigin src="/react/index-BVksWkHS.js"></script>
     <link rel="modulepreload" crossorigin href="/react/rolldown-runtime-CNC7AqOf.js">
     <link rel="modulepreload" crossorigin href="/react/@ant-design-DAiAtinM.js">
     <link rel="modulepreload" crossorigin href="/react/@docuninja-CiT7sB1w.js">
@@ -71,7 +71,7 @@
     <link rel="modulepreload" crossorigin href="/react/react-number-format-Cs46NX1-.js">
     <link rel="modulepreload" crossorigin href="/react/react-use-B3obNexs.js">
     <link rel="modulepreload" crossorigin href="/react/NumberInputField-CUTlUHom.js">
-    <link rel="modulepreload" crossorigin href="/react/product-reservation-i18n-Ba1ZmwNy.js">
+    <link rel="modulepreload" crossorigin href="/react/product-reservation-i18n-GdPuoDah.js">
     <link rel="modulepreload" crossorigin href="/react/data-table-BxvH3TdV.js">
     <link rel="modulepreload" crossorigin href="/react/Plus-DFbhciZE.js">
     <link rel="modulepreload" crossorigin href="/react/Combobox-7f-ZUVQl.js">
@@ -105,7 +105,7 @@
     <link rel="modulepreload" crossorigin href="/react/invoices-DQlBoRf5.js">
     <link rel="modulepreload" crossorigin href="/react/useFormatNumber-DNYGaXyv.js">
     <link rel="modulepreload" crossorigin href="/react/products-JKDOo9yf.js">
-    <link rel="modulepreload" crossorigin href="/react/ProductSelector-BBaOBJJl.js">
+    <link rel="modulepreload" crossorigin href="/react/ProductSelector-9MO5LtDY.js">
     <link rel="modulepreload" crossorigin href="/react/collect.js-Dg3gyr1N.js">
     <link rel="modulepreload" crossorigin href="/react/ChevronUp-GWMUeku7.js">
     <link rel="modulepreload" crossorigin href="/react/tables-CYc09fki.js">
@@ -115,7 +115,7 @@
     <link rel="modulepreload" crossorigin href="/react/hex-color-regex-CVuZZM3o.js">
     <link rel="modulepreload" crossorigin href="/react/StatusColorTheme-WmXM3WTj.js">
     <link rel="modulepreload" crossorigin href="/react/inclusive-tax-HkhZ7N2P.js">
-    <link rel="modulepreload" crossorigin href="/react/invoice-sum-inclusive-sBd6V2nT.js">
+    <link rel="modulepreload" crossorigin href="/react/invoice-sum-inclusive-R4iM4QG9.js">
     <link rel="modulepreload" crossorigin href="/react/payment-terms-OOLwVviM.js">
     <link rel="modulepreload" crossorigin href="/react/CountrySelector-nd606Oc3.js">
     <link rel="modulepreload" crossorigin href="/react/CurrencySelector-Dh3_l4nV.js">
---
+++ tests/Unit/ProductGroupPersistenceTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Product;
+use App\Repositories\ProductRepository;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class ProductGroupPersistenceTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->string('product_key');
+            $table->boolean('is_group')->default(false);
+            $table->boolean('group_hide_item_prices')->default(false);
+            $table->boolean('group_has_price')->default(false);
+            $table->decimal('group_price', 16, 4)->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        Schema::create('product_group_items', function (Blueprint $table) {
+            $table->unsignedBigInteger('group_product_id');
+            $table->unsignedBigInteger('product_id');
+            $table->decimal('quantity', 16, 4);
+            $table->unsignedInteger('sort_id');
+        });
+    }
+
+    public function testGroupMembersSurviveSaveAndReload(): void
+    {
+        $groupId = DB::table('products')->insertGetId([
+            'company_id' => 1,
+            'product_key' => 'group',
+        ]);
+        $childId = DB::table('products')->insertGetId([
+            'company_id' => 1,
+            'product_key' => 'child',
+        ]);
+        $group = Product::query()->findOrFail($groupId);
+
+        Product::withoutEvents(fn () => (new ProductRepository())->save([
+            'is_group' => true,
+            'group_items' => [[
+                'product_id' => $childId,
+                'quantity' => 2.5,
+            ]],
+        ], $group));
+
+        $member = Product::query()->findOrFail($groupId)->group_products()->first();
+
+        $this->assertSame($childId, $member->id);
+        $this->assertSame(2.5, (float) $member->pivot->quantity);
+    }
+}
