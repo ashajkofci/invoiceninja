6f62c06431ede66ba13a7e633176f5d4e395a240
Adrian Shajkofci <adrianshajkofci@MacBook-Air-de-Adrian.local>
2026-09-13 22:18:15 +0200

    new version

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3055040250" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1213227283" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
